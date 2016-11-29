299d6b17b2f52c230f873ffc526a1a0968808712
WIldan firdaus <firdauswildan87@gmail.com>
2016-11-29 15:57:51 +0700

    Ini adalah projek ohm meter

---
+++ Project_Ohmmeter_21.ino
@@ -0,0 +1,32 @@
+/* Projek Ohm Meter
+ Wildan Firdaus
+ 4211501021
+ MK - 3A
+ */
+
+const int pinADC = A0;
+float nilaiADC = 0; 
+float Vin = 5; //volt
+float Vout = 0;//volt
+float R1 = 56; //ohm
+float R2 = 0; //ohm
+
+void setup ()
+{
+  Serial.begin(9600);
+}
+
+void loop ()
+{
+  nilaiADC = analogRead(pinADC);  // baca Vout pada analog input pin A0
+  Vout = (Vin*nilaiADC)/1023;    // konversi Vout ke volt
+  R2 = ((Vin/Vout)-1) * R1;
+  Serial.print("Nilai ADC =  ");
+  Serial.println(nilaiADC);
+  Serial.print("Vout =  "); //volt
+  Serial.println(Vout);
+  Serial.print("R2 =  "); //ohm                  
+  Serial.println(R2); 
+    Serial.print("\n");
+  delay(1500);       
+}
